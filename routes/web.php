e7004585174738397146d498d61ba717214628ac
Edward-Kabue <ekabue0@gmail.com>
2024-02-09 11:40:16 +0300

    Add laravel breeze to the project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TasksController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,7 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('tasks', TasksController::class);
-//Route::get('/tasks', [TasksController::class, 'index']);
-//Route::post('/tasks', [TasksController::class, 'delete']);
-//Route::post('/tasks', [TasksController::class, 'update']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
